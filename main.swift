f158d21d352840816e1f24bc685823796edcaef3
marakitemesgen <marafun9@gmail.com>
2024-07-10 15:59:50 -0400

    add solution 1

---
+++ main.swift
@@ -0,0 +1,32 @@
+/ Online Swift compiler to run Swift program online
+// Print "Try programiz.pro" message
+
+// Constants
+let appName: String = "MarakiApp"
+let developerName: String = "Maraki T"
+let initialReleaseyear: Int = 2016
+
+
+//Variable
+var Currentversion: Double = 1.0
+var numberOfverison: Int = 5000
+var userRating: Double = 4.0
+
+// Print intital data
+print ("App Name: \(appName)")
+print ("Developer Name: \(developerName)")
+print ("Initial Release Year: \(initialReleaseyear)")
+print ("Current Version: \(Currentversion)")
+print ("Number of Verisons: \(numberOfverison)")
+print ("Users Rating : \(userRating)")
+
+// Stimulate an Update
+Currentversion += 0.1
+numberOfverison += 1000
+userRating += 0.2
+
+//Updated in
+print ("Updated Current Version: \(Currentversion)")
+print ("Updated Number of Verisons: \(numberOfverison)")
+print ("Updated Users Rating: \(userRating)")
+
