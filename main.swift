5a8473c594a4fac9527402c83510c0a5099ba08f
marakitemesgen <marafun9@gmail.com>
2024-07-11 15:25:42 -0400

    Update main.swift

--- main.swift
+++ main.swift
@@ -30,3 +30,39 @@ print ("Updated Current Version: \(Currentversion)")
 print ("Updated Number of Verisons: \(numberOfverison)")
 print ("Updated Users Rating: \(userRating)")
 
+var postiveReviews: Int = 90
+var negativeReviews: Int = 15
+let totalReviews: Int = 
+postiveReviews + negativeReviews
+print ("Total Reviews: \(totalReviews)")
+
+//Calculate new rating
+userRating += Double (postiveReviews) * 0.1
+userRating -= Double (negativeReviews) * 0.1
+
+print ("New User Rating: \(userRating)")
+
+var recommendation: String
+
+if userRating >= 4.5 {
+    recommendation = "Highly Recommended"
+} 
+
+else if userRating >= 3.5 {
+    recommendation = "Recommended"
+}
+  
+  else if userRating >= 2.5 {
+    recommendation = "Moderately Recommended"
+}
+    
+  else {
+    recommendation = "Not Recommended"
+
+    }
+    
+print("Recommendation: \(recommendation)")
+
+
+
+
